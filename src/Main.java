e1b8e85f70dd11768c5bef80c20e731be3a8257d
Alexey Prokhorov <alexeyprokhorov@outlook.com>
2020-09-26 13:58:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("here's first 10 harmonic numbers");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(1.0 / i + "\t");
+        }
+    }
+}
